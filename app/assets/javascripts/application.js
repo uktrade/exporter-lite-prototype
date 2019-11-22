647062eb8202e85e5cf0de0c62b89ebc4f8dc20b
Enor Anidi-Ryz <hello@enoranidi.com>
2019-11-22 16:24:49 +0000

    Home page changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,13 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+$('a.more-link').click(function(ev){
+    ev.preventDefault();
+    
+    // hide everything
+    $('.box').not(
+        // except the corresponding box
+        $('.box.'+$(this).data('box')+':hidden').fadeIn()
+    ).fadeOut(); 
+});
